6793aad1a1e5d298c326cd35445929d2acb9e7cf
Sean <seanwilliamogara@gmail.com>
2024-04-17 10:11:43 +0100

    front end styling

--- app.py
+++ app.py
@@ -15,18 +15,50 @@ def file_uploader(cont, label):
             with open(uploaded_file.name, mode='wb') as w:
                 w.write(uploaded_file.getvalue())
             return chat_bot1._load(uploaded_file.name)
-
+        
 
 st.set_page_config(layout="wide")
-st.markdown("<h1 style='text-align: center; color: black; top: -10'>Bill Challenger</h1>", unsafe_allow_html=True)
+
+col1, col2, col3, col4, col5 = st.columns(5)
+
+with col1:
+    st.write('')
+
+with col2:
+    st.write('')
+    #st.markdown("<h1 style='text-align: center; top: -10'>Legal (AI)d</h1>", unsafe_allow_html=True)
+with col3:
+    st.write('')
+    st.write('')
+    st.write('')
+    # Title wrapped in h1 with CSS class
+    title_text = "Legal <span style='color:green'>AI</span>d"
+    st.markdown(f"<h1 class='custom-title'>{title_text}</h1>", unsafe_allow_html=True)
+
+    # CSS to style the title element (replace with your font source)
+    st.markdown("""
+    <style>
+    .custom-title {
+    font-family: 'Noto Sans Gurmukhi', sans-serif;
+    font-size: 48px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+with col4:
+    st.write('')
+
+with col5:
+    st.image("logo_hackathon.png", width=200)
+
+#st.markdown("<h1 style='text-align: center; top: -10'>Legal (AI)d</h1>", unsafe_allow_html=True)
             # <span style="font-family: Lora; font-size: 1em;">
-st.markdown("""
-            ##### This app enables:
+# st.markdown("""
+#             ##### This app enables:
     
-                1. Checking consistency between bills and their explanatory notes.
-                2. Finding past bills relevant to a given legislation, and find possible aspects missed in the given legislation as discussed in each of the relevant ones.
-                3. Finding the past bills that may contradict with the proposals of the given legislation.  
-            """, unsafe_allow_html=True)
+#                 1. Checking consistency between bills and their explanatory notes.
+#                 2. Finding past bills relevant to a given legislation, and find possible aspects missed in the given legislation as discussed in each of the relevant ones.
+#                 3. Finding the past bills that may contradict with the proposals of the given legislation.  
+#             """, unsafe_allow_html=True)
 
 with st.container(height=190, border=True):
     st.markdown("#### Uploads")
@@ -36,7 +68,7 @@ with st.container(height=190, border=True):
 
 with st.container(height=310, border=True):
     st.markdown("#### Analysis")
-    tab1, tab2 = st.tabs(["Consistency Checker", "Relevance+Coverage Checker"])#, "Contradiction Checker"])
+    tab1, tab2 = st.tabs(["Consistency Checker", "Relevance + Coverage Checker"])#, "Contradiction Checker"])
     # task 1
     with tab1:
         st.markdown("##### Found inconsistencies:")
